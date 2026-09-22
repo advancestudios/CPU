54b62ded78b7df71c2d53a2cb10d85635ce79cd3
AdvanceStudios <sradvance1101@gmail.com>
2026-09-22 15:35:02 -0400

    Update log message in ready event

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
-        console.log(`🚀 [CPU v2] Núcleo operativo inicializado y activo como ${client.user.tag}`);
+        console.log(`🚀 CPU Núcleo operativo inicializado y activo como ${client.user.tag}`);
     }
 };
