e36db4d49731f51ad26a637c4805f48a7ddd604b
AdvanceStudios <sradvance1101@gmail.com>
2026-09-23 08:35:35 -0400

    Change warning embed color from red to yellow

--- commands/warn.js
+++ commands/warn.js
@@ -37,7 +37,7 @@ module.exports = {
 
             const embed = new EmbedBuilder()
                 .setTitle('<:warn_icon:1552150324154998804> Miembro Advertido')
-                .setColor('#ED4245')
+                .setColor('#ffae00')
                 .setThumbnail(usuario.user.displayAvatarURL({ dynamic: true }))
                 .addFields(
                     { name: 'Miembro Advertido', value: `${usuario.user.username}`, inline: true },
