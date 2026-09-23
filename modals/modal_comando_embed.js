03741bb7f0f4f092edb421438fef0941839e4486
AdvanceStudios <sradvance1101@gmail.com>
2026-09-23 08:38:33 -0400

    Change accent color in ContainerBuilder to transparent

--- modals/modal_comando_embed.js
+++ modals/modal_comando_embed.js
@@ -8,7 +8,7 @@ module.exports = {
         const footer = interaction.fields.getTextInputValue('input_embed_footer');
 
         try {
-            const container = new ContainerBuilder().setAccentColor(0x5865F2);
+            const container = new ContainerBuilder().setAccentColor(#00000000);
 
             // 1. Título principal (sin separador propio: no mete espacio de más)
             if (titulo && titulo.trim().length > 0) {
